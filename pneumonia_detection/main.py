e129b9111f9d442cd662580995516c6b3e2c072f
Siyam <bhuiyansiyam294@gmail.com>
2025-08-14 18:14:02 +0600

    added training modes for CNN with and without augmentation in main file

--- pneumonia_detection/main.py
+++ pneumonia_detection/main.py
@@ -1,22 +1,40 @@
+import argparse
 import torch
 from pneumonia_detection.CNN.cnn_trainer import run_training
-from pneumonia_detection.CNN.model import PneumoniaCNN
-from pneumonia_detection.dataset import get_dataloaders
 from pneumonia_detection.config import MODEL_DIR
+from pneumonia_detection.CNN.model import PneumoniaCNN
+from pneumonia_detection.dataset import (
+    get_dataloaders_cnn_no_aug,
+    get_dataloaders_cnn_aug,
+)
+
+def train_cnn_no_aug(save_path="models/best_cnn_model_noaug.pth"):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device} | Mode: CNN without augmentation")
+    train_loader, val_loader, _ = get_dataloaders_cnn_no_aug()
+
+    model = PneumoniaCNN().to(device)
+    run_training(model, train_loader, val_loader, device, save_path=save_path)
+    print(f"Done. Saved: {save_path}")
+
+def train_cnn_with_aug(save_path="models/best_cnn_model_aug.pth", use_weighted_sampler=True):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device} | Mode: CNN with augmentation")
+    train_loader, val_loader, _ = get_dataloaders_cnn_aug(use_weighted_sampler=use_weighted_sampler)
+
+    model = PneumoniaCNN().to(device)
+    run_training(model, train_loader, val_loader, device, save_path=save_path)
+    print(f"Done. Saved: {save_path}")
 
 def main():
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print(f'Using device: {device}')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mode", choices=["noaug", "aug"], default="noaug")
+    args = parser.parse_args()
 
-    MODEL_DIR.mkdir(exist_ok=True, parents=True)
+    if args.mode == "aug":
+        train_cnn_with_aug()
+    else:
+        train_cnn_no_aug()
 
-    train_loader, val_loader, test_loader = get_dataloaders()
-    model = PneumoniaCNN().to(device)
-
-    save_path = (MODEL_DIR / "best_cnn_model.pth").as_posix()
-    run_training(model, train_loader, val_loader, device, save_path=save_path)
-
-    print('Training completed! Best model saved at:', save_path)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
