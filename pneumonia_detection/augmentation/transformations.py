be8ea82b8ec82c45cbfc87be73ec055ee9a300cc
Siyam <bhuiyansiyam294@gmail.com>
2025-08-14 18:20:10 +0600

    enhancing training augmentations with additional transformations (sharpness, autocontrast etc

--- pneumonia_detection/augmentation/transformations.py
+++ pneumonia_detection/augmentation/transformations.py
@@ -1,21 +1,37 @@
 from torchvision import transforms
 from pneumonia_detection.config import IMAGE_SIZE
 
-# Training-time augmentation (safe for chest X-rays)
-train_transform = transforms.Compose([
-    transforms.Grayscale(num_output_channels=1),
-    transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
-    transforms.RandomHorizontalFlip(p=0.5),
-    transforms.RandomRotation(degrees=3),
-    transforms.RandomAffine(degrees=0, translate=(0.03, 0.03)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5], std=[0.5]),
-])
 
-# Validation/Test (no augmentation)
-test_val_transform = transforms.Compose([
-    transforms.Grayscale(num_output_channels=1),
-    transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5], std=[0.5]),
-])
+#preprocessing for val/test (no-aug)
+def build_eval_transform():
+    return transforms.Compose([
+        transforms.Grayscale(num_output_channels=1),
+        transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5], std=[0.5]),
+    ])
+
+
+#augmentations for training
+def build_train_augment_transform():
+    return transforms.Compose([
+        transforms.Grayscale(num_output_channels=1),
+        transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
+
+        transforms.RandomAffine(
+            degrees=5,              # tiny rotations
+            translate=(0.04, 0.04), # <= 4% shifts
+            scale=(0.98, 1.02),     # light zoom
+            shear=(-2, 2),
+        ),
+        transforms.RandomHorizontalFlip(p=0.5),
+
+    
+        transforms.RandomAdjustSharpness(1.2, p=0.25),
+        transforms.RandomAutocontrast(p=0.25),
+        transforms.RandomEqualize(p=0.15),
+        transforms.GaussianBlur(kernel_size=3, sigma=(0.1, 1.0)),
+
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5], std=[0.5]),
+    ])
